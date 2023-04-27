e407fc9a6d4c207632f2b084fcc391e572dafa49
ff6347 <hash@ff6347.email>
2023-04-27 21:32:41 +0200

    test: Use single quote

--- __tests__/shell.test.sh
+++ __tests__/shell.test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC2016
 set -u pipefail
 IFS=$'\n\t'
-
 output=$(echo "<h1>foo</h1>" | html2md)
 exitcode=1
 if echo "$output" | grep -q "# foo"; then
@@ -14,7 +14,7 @@ else
   exitcode=1
 fi
 output=$(html2md -c)
-if echo "$output" | grep -q "Couldn't find the \`xsel\` binary and fallback didn't work. On Debian/Ubuntu you can install xsel with: sudo apt install xsel\n"; then
+if echo "$output" | grep -q 'Couldn'\''t find the `xsel` binary and fallback didn'\''t work. On Debian/Ubuntu you can install xsel with: sudo apt install xsel'; then
   echo "Test passed xsel"
   echo "$output"
   exitcode=0
