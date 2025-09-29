3d7219a218ce154ceed38824f4f43be7306b6400
Fabian Morón Zirfas <hash@ff6347.email>
2025-09-29 11:53:50 +0200

    style: Formatting

--- src/turndown-plugin-gfm.d.ts
+++ src/turndown-plugin-gfm.d.ts
@@ -1,39 +1,38 @@
-declare module 'turndown-plugin-gfm' {
-	import TurndownService from 'turndown';
-  
+declare module "turndown-plugin-gfm" {
+	import TurndownService from "turndown";
+
 	type Filter = string | string[] | ((node: HTMLElement) => boolean);
 	type ReplacementFunction = (
-	  content: string,
-	  node: HTMLElement,
-	  options: { fence: string }
+		content: string,
+		node: HTMLElement,
+		options: { fence: string },
 	) => string;
-  
+
 	interface Rule {
 		filter: Filter;
 		replacement?: (
-		  content: string,
-		  node: HTMLElement,
-		  options: TurndownService.Options,
-		  captureGroups: string[]
+			content: string,
+			node: HTMLElement,
+			options: TurndownService.Options,
+			captureGroups: string[],
 		) => string | void | null;
 		options?: TurndownService.Options;
-	  }
-  
+	}
+
 	function highlightedCodeBlock(turndownService: any): void;
 	function strikethrough(turndownService: any): void;
 	function tables(turndownService: any): void;
 	function taskListItems(turndownService: any): void;
 	function gfm(turndownService: any): void;
-  
+
 	export {
-	  highlightedCodeBlock,
-	  strikethrough,
-	  tables,
-	  taskListItems,
-	  gfm,
-	  Rule,
-	  Filter,
-	  ReplacementFunction,
+		highlightedCodeBlock,
+		strikethrough,
+		tables,
+		taskListItems,
+		gfm,
+		Rule,
+		Filter,
+		ReplacementFunction,
 	};
-  }
-  
+}
